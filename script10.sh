95101641aee7c29c37bbb754875293f169be8d42
m-chiranjeevi1 <93828368+m-chiranjeevi1@users.noreply.github.com>
2022-02-13 13:30:12 +0530

    Create script10.sh

---
+++ script10.sh
@@ -0,0 +1,21 @@
+#!/bin/bash  
+  
+<<COMMENTS  
+    This is the first comment  
+    This is the second comment  
+    This is the third comment  
+COMMENTS  
+  
+echo "Hello World"  
+
+or
+
+
+
+: '  
+This is the first comment  
+This is the second comment  
+This is the third comment  
+'  
+  
+echo "Hello World"  
