b7ba5266f4a143b440e71839528d2f8262c2ea10
Ubuntu <ubuntu@ip-172-31-37-197.ap-south-1.compute.internal>
2022-02-23 07:10:30 +0000

    02

---
+++ .bash_history
@@ -0,0 +1,73 @@
+ls
+vi installsoft.sh 
+sh installsoft.sh n
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+cls
+clear
+sh installsoft.sh 
+vi vi installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+uname -n
+uname -a
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+uname -a
+Ubuntu
+vi installsoft.sh 
+sh installsoft.sh 
+vi installsoft.sh 
+sh installsoft.sh 
+ls
+cp installsoft.sh test.sh 
+vi test.sh 
+sh test.sh 
+vi test.sh 
+sh test.sh 
+vi test.sh 
+ls
+./installsoft.sh 
+vi installsoft.sh 
+./installsoft.sh 
+java -version
+./installsoft.sh 
+vi installsoft.sh 
+uname -a
+uname -o
+uname
+uname --help
+uname -v
+uname -s
+uname -a
+ls
+vi installsoft.sh 
---
+++ test.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+
+echo -e "enter a number to install software\n1.java\n2.git\n3.maven"
+read num
+
+case $num in
+	1)
+		
+		echo "Option 1 enterted "	
+	      	osname=$(uname -a)
+		echo  " $osname == *Ubuntu* "
+		if [[ "$osname" == *"Ubuntu"* ]]
+		then
+		  echo -e "it's ubuntu\ninstalling java"
+		  sudo apt-get -y update
+		  sudo apt-get install -y openjdk-8-jre-headless
+		  java -version
+	        elif [[ $osname == *Linux* ]]
+	        then	   
+		   echo -e "it's linux\ninstalling java"
+		   sudo yum -y update
+		   sudo yum install -y java-1.8.0-openjdk.x86_64
+		   java -version
+	        else
+                   echo "unable to install because its not ubuntu or linux"
+		fi
+		echo
+		;;
+
+	2)
+		 echo "installing git"
+		 osname=$(uname -a) 
+		 if [[ $osname == *Ubuntu* ]]
+                 then
+                   echo -e "it's ubuntu\ninstalling git"
+                   sudo apt-get -y update
+	           sudo apt-get install -y git
+		   git --version
+	         elif [[ $osname == *Linux* ]]
+		 then
+		   echo -e "it's linux\ninstalling git"
+                   sudo yum -y update
+                   sudo yum install -y git
+                   git --version
+
+		  
+		fi
+		;;
+
+	3)
+		echo "installing maven"
+		sudo apt-get install -y maven
+		mvn --version
+		echo
+		;;
+
+     
+esac	
